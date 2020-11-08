8ba31c7f335edebee007122cee75083709c7cf53
stupid-yu <hui.yu0719@outlook.com>
2020-11-09 00:00:10 +0800

    week06

---
+++ Week_06/647.回文子串.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=647 lang=java
+ *
+ * [647] 回文子串
+ */
+
+// @lc code=start
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length(), ans = 0;
+        for (int i = 0; i < 2 * n - 1; ++i) {
+            int l = i / 2, r = i / 2 + i % 2;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                --l;
+                ++r;
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
